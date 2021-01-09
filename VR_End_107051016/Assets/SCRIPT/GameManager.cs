79d553bdec6ff79d244624d38d0bbe26c9e29c54
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2021-01-10 00:34:09 +0800

    FINISH

---
+++ VR_End_107051016/Assets/SCRIPT/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("保齡球場");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ VR_End_107051016/Assets/SCRIPT/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScroe;
+
+    [Header("分數")]
+    public int score;
+
+    [Header("投進分數")]
+    public int scorein = 1;
+
+    [Header("進球音效")]
+    public AudioClip soundin;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "保齡球瓶" && other.transform.position.y < 0.75f)
+        {
+            AddScroe();
+        }
+        
+    }
+    
+    private void AddScroe()
+    {
+        score += scorein;
+        textScroe.text =  score+ "/10";
+        aud.PlayOneShot(soundin, Random.Range(1f, 10f));
+
+    }
+
+}
